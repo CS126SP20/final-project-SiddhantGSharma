618c009ef72590450335456c04321e13c2381c2d
Siddhant <sharma63@illinois.edu>
2020-04-22 09:57:46 +0530

    Added the SQLite3 and sqlite-modern-cpp libraries correctly to the project.

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -1,3 +1,8 @@
 # Development
 
 ---
+### Week 1
+**04/21/20**
+1. Included sqlite-modern-cpp correctly.
+2. Included sqlite3 correctly.
+3. Added header files properly.
--- include/mylibrary/example.h
+++ include/mylibrary/example.h
@@ -1,10 +1,10 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 #ifndef FINALPROJECT_MYLIBRARY_EXAMPLE_H_
 #define FINALPROJECT_MYLIBRARY_EXAMPLE_H_
-
+#include <sqlite_modern_cpp.h>
+#include <sqlite3.h>
 
 namespace mylibrary {
-
 }  // namespace mylibrary
 
 
