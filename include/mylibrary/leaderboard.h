9440d6f5cc1e580f14a7ace1fa150fe6d91a1a38
Siddhant <sharma63@illinois.edu>
2020-04-27 16:58:56 +0530

    Added leaderboard.cc and leaderboard.h to maintain leaderboard in the game.

--- include/mylibrary/leaderboard.h
+++ include/mylibrary/leaderboard.h
@@ -5,4 +5,31 @@
 #ifndef FINALPROJECT_LEADERBOARD_H
 #define FINALPROJECT_LEADERBOARD_H
 
+#include "player.h"
+
+#include <sqlite_modern_cpp.h>
+
+#include <string>
+#include <vector>
+
+namespace hangman {
+
+class LeaderBoard {
+ public:
+  // Creates a new leaderboard table if it doesn't already exist.
+  explicit LeaderBoard(const std::string& database_path);
+
+  // Adds a player to the leaderboard.
+  void AddScoreToLeaderBoard(const Player&);
+
+  // Returns a list of the players with the highest scores, in decreasing order.
+  // The size of the list should be no greater than `limit`.
+  std::vector<Player> RetrieveHighScores(const size_t limit);
+
+ private:
+  sqlite::database database_;
+};
+
+}
+
 #endif  // FINALPROJECT_LEADERBOARD_H
