f64ac00936560d69afd59e5900f4763b57efc283
Siddhant <sharma63@illinois.edu>
2020-05-02 23:40:07 -0500

    Added code to return a single movie from a list.

--- include/mylibrary/game_engine.h
+++ include/mylibrary/game_engine.h
@@ -5,10 +5,29 @@
 #ifndef FINALPROJECT_GAME_ENGINE_H
 #define FINALPROJECT_GAME_ENGINE_H
 
+#include "read_file.h"
+
 namespace hangman {
   class Engine {
    public:
+    Engine();
+    std::string GetMovieFromList();
+    bool PlayRound(std::string movie_name);
+    void SetCharacter(char user_guess);
+    bool IsCorrectGuess(char user_guess, std::string movie_name);
+    int getScore() const;
+    void setScore(int score);
+    int getIncorrectGuess() const;
+    void setIncorrectGuess(int incorrectGuess);
+    char getUserGuess() const;
+    void setUserGuess(char userGuess);
 
+   private:
+    int score_;
+    const char* getMovieName() const;
+    int incorrect_guess_;
+    char user_guess_;
+    char movie_name_[50];
   };
 }
 
--- src/game_engine.cc
+++ src/game_engine.cc
@@ -2,6 +2,49 @@
 // Created by SIDDHANT on 4/27/2020.
 //
 
+#include <mylibrary/game_engine.h>
+
 namespace hangman {
+
+Engine::Engine() {}
+
+std::string Engine::GetMovieFromList() {
+  ReadFile readFile;
+  std::vector<std::string> movies_list = readFile.ReadFile::ReadTxtFile("hello.txt");
+
+  if (movies_list.empty()) {
+    return "Game Over";
+  }
+
+  std::string movie = movies_list.back();
+  movies_list.pop_back();
+  return movie;
+}
+
+bool Engine::PlayRound(std::string movie_name) {
+  return false;
+}
+
+void Engine::SetCharacter(char user_guess) {}
+
+bool Engine::IsCorrectGuess(char user_guess, std::string movie_name) {
+  return false;
+}
+
+int Engine::getScore() const { return score_; }
+
+void Engine::setScore(int score) { score_ = score; }
+
+int Engine::getIncorrectGuess() const { return incorrect_guess_; }
+
+void Engine::setIncorrectGuess(int incorrectGuess) {
+    incorrect_guess_ = incorrectGuess;
+}
+
+char Engine::getUserGuess() const { return user_guess_; }
+
+void Engine::setUserGuess(char userGuess) { user_guess_ = userGuess; }
+
+const char* Engine::getMovieName() const { return movie_name_; }
 
 }
