8384c2c8f80b97da20c6962d042463d9dde9a4cc
Siddhant <sharma63@illinois.edu>
2020-05-05 13:10:37 -0500

    Added more test cases for game_engine.cc

--- tests/test.cc
+++ tests/test.cc
@@ -3,11 +3,9 @@
 #define CATCH_CONFIG_MAIN
 
 #include <catch2/catch.hpp>
-#include <cinder/Rand.h>
 
 #include <mylibrary/game_engine.h>
 #include <mylibrary/read_file.h>
-#include <mylibrary/leaderboard.h>
 
 using std::string;
 using std::vector;
@@ -51,6 +49,51 @@ TEST_CASE("Create and get movie from list") {
   }
 }
 
+TEST_CASE("Run a step in game") {
+  hangman::Engine engine;
+  engine.CreateList();
+
+  SECTION("Playing a round") {
+    engine.setUserGuess('d');
+    engine.Step();
+    std::vector<char> testing_vector{'U', '-', 'd', 'e', '-', ' ', '-', '-', 'e', ' ', '-', 'e', 'd', '-', 'o', 'o', 'd'};
+
+    REQUIRE(engine.getIncompleteMovieName() == testing_vector);
+  }
+
+  std::vector<char> testing_vector{'U', 'n', 'd', 'e', 'r', ' ', 'T', 'h', 'e', ' ', 'R', 'e', 'd', 'h', 'o', 'o', 'd'};
+  engine.setIncompleteMovieName(testing_vector);
+  engine.Step();
+
+  REQUIRE(engine.isRoundOver());
+  REQUIRE(engine.getScore() == 17);
+}
+
+TEST_CASE("Play round") {
+  hangman::Engine engine;
+  engine.CreateList();
+
+  SECTION("Correct user guess") {
+    engine.setUserGuess('d');
+    engine.PlayRound();
+    std::vector<char> testing_vector{'U', '-', 'd', 'e', '-', ' ', '-', '-', 'e', ' ', '-', 'e', 'd', '-', 'o', 'o', 'd'};
+
+    REQUIRE(engine.getIncompleteMovieName() == testing_vector);
+  }
+
+  SECTION("Incorrect user guess") {
+    engine.setUserGuess('1');
+    engine.PlayRound();
+    std::vector<char> testing_vector{'U', '-', '-', 'e', '-', ' ', '-', '-', 'e', ' ', '-', 'e', '-', '-', 'o', 'o', '-'};
+
+    REQUIRE(engine.getIncompleteMovieName() == testing_vector);
+    REQUIRE(engine.getIncorrectGuesses().size() == 1);
+
+    std::vector<char> testing_vector_two{'1'};
+    REQUIRE(engine.getIncorrectGuesses() == testing_vector_two);
+  }
+}
+
 TEST_CASE("Is correct guess") {
   hangman::Engine engine;
   engine.CreateList();
@@ -81,4 +124,45 @@ TEST_CASE("Is a character vowel") {
   REQUIRE(engine.isVowel('a'));
   REQUIRE_FALSE(engine.isVowel('z'));
   REQUIRE_FALSE(engine.isVowel('3'));
+}
+
+TEST_CASE("Game over") {
+  SECTION("Game over due to guessing all movie names") {
+    hangman::Engine engine;
+    engine.CreateList();
+
+    while(!engine.getMoviesList().empty()) {
+      engine.GetMovieFromList();
+    }
+
+    engine.GetMovieFromList();
+    REQUIRE(engine.isGameOver());
+  }
+
+  SECTION("Game over due to incorrect guess") {
+    hangman::Engine engine;
+    engine.setUserGuess('1');
+    engine.PopulateIncorrectGuessList();
+    engine.setUserGuess('2');
+    engine.PopulateIncorrectGuessList();
+    engine.setUserGuess('3');
+    engine.PopulateIncorrectGuessList();
+    engine.setUserGuess('4');
+    engine.PopulateIncorrectGuessList();
+    engine.setUserGuess('5');
+    engine.PopulateIncorrectGuessList();
+
+    REQUIRE_FALSE(engine.isGameOver());
+
+    engine.setUserGuess('6');
+    engine.PopulateIncorrectGuessList();
+    engine.setUserGuess('7');
+    engine.PopulateIncorrectGuessList();
+    engine.setUserGuess('8');
+    engine.PopulateIncorrectGuessList();
+    engine.setUserGuess('5');
+    engine.PopulateIncorrectGuessList();
+
+    REQUIRE(engine.isGameOver());
+  }
 }
